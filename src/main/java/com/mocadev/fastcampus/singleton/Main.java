c428879af857cb90270814b318f7b76164bea08d
tony <chcjswo@gmail.com>
2021-09-07 22:48:31 +0900

    패스트캠퍼스 디자인패턴 공부 시작

---
+++ src/main/java/com/mocadev/fastcampus/singleton/Main.java
@@ -0,0 +1,16 @@
+package com.mocadev.fastcampus.singleton;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-09-07
+ **/
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("args = " + args);
+	}
+
+}
