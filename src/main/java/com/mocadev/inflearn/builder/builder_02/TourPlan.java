34f112b2acc93d8e04c14e025e163125c9c6dc39
tony <chcjswo@gmail.com>
2021-11-06 00:12:11 +0900

    빌더 패턴

--- src/main/java/com/mocadev/inflearn/builder/builder_02/TourPlan.java
+++ src/main/java/com/mocadev/inflearn/builder/builder_02/TourPlan.java
@@ -12,9 +12,32 @@ import java.util.List;
  **/
 public class TourPlan {
 
+	private String title;
+	private int nights;
+	private int days;
+	private LocalDate startDate;
+	private String whereToStay;
+	private List<DetailPlan> plans;
+
 	public TourPlan(String title, int nights, int days, LocalDate startDate, String whereToStay,
 					List<DetailPlan> plans) {
-
+		this.title = title;
+		this.nights = nights;
+		this.days = days;
+		this.startDate = startDate;
+		this.whereToStay = whereToStay;
+		this.plans = plans;
 	}
 
+	@Override
+	public String toString() {
+		return "TourPlan{" +
+			"title='" + title + '\'' +
+			", nights=" + nights +
+			", days=" + days +
+			", startDate=" + startDate +
+			", whereToStay='" + whereToStay + '\'' +
+			", plans=" + plans +
+			'}';
+	}
 }
